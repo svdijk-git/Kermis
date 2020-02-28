092fbda5cd6bee69cc5dd8f4c54766a2b5b74948
Sjors <s.j.vandijk93@gmail.com>
2020-02-28 15:43:29 +0100

    KermisKaart

--- KermisKaart.java
+++ KermisKaart.java
@@ -1,16 +1,24 @@
 package com.les;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class KermisKaart {
-private String naam;
-private List<Attractie> attracties;
+    private String naam;
+    private List<Attractie> attracties;
 
-    public KermisKaart (String ParkNaam){
+    public KermisKaart(String ParkNaam) {
         this.naam = ParkNaam;
         this.attracties = new ArrayList<Attractie>();
 
+    }
 
+    public String getNaam() {
+        return this.naam;
+    }
+
+    public void addAttractie(Attractie attractie) {
+        this.attracties.add(attractie);
     }
 }
